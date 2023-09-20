c2ae36f1ffa0f687cc6c345bc06d852f7c96cb63
GemotiveerdeStudent <129509756+GemotiveerdeStudent@users.noreply.github.com>
2023-09-20 10:38:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MatchdayAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
